d760a925d403abfab40156ae5b48bbf955479c2e
Thiago Franca <francasantos.t@gmail.com>
2025-09-15 22:21:18 +0000

    dtos para put e post criados e funcionais

---
+++ src/user/user.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() { email, name, password }: CreateUserDto) {
+    return { email, name, password };
+  }
+
+  @Get()
+  async read() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async readOne(@Param() params) {
+    return { user: {}, params };
+  }
+
+  @Put(':id')
+  async update(
+    @Body() { email, name, password }: UpdatePutUserDTO,
+    @Param() params,
+  ) {
+    return {
+      method: 'put',
+      email,
+      name,
+      password,
+      params,
+    };
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params) {
+    return {
+      method: 'patch',
+      body,
+      params,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param() params) {
+    return {
+      params,
+    };
+  }
+}
